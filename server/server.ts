2762918e3a6e3e14a98c113a9961a2df7eec6fd2
Doriel Shacham <97340087+DorielShacham@users.noreply.github.com>
2024-01-24 20:17:06 +0200

    adding args to pupter

--- server/server.ts
+++ server/server.ts
@@ -52,7 +52,7 @@ app.post('/api/posts/:creatorId', async (req, res) => {
     const { url } = req.body;
 
     console.log('Launching browser...');
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'], });
     console.log('Browser launched successfully.');
     const page = await browser.newPage();
 
