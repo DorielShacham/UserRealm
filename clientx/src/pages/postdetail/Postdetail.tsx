bd5934aa3d189e8e77cfb458bd4c60080816023e
Doriel Shacham <97340087+DorielShacham@users.noreply.github.com>
2024-03-10 11:28:02 +0200

    copy link different location and text

--- clientx/src/pages/postdetail/Postdetail.tsx
+++ clientx/src/pages/postdetail/Postdetail.tsx
@@ -80,11 +80,12 @@ export const Postdetail = () => {
               <div className="post-detail__action-buttons">
                 <LikeButton postId={id || ""} currentUser={currentUser} className="post-detail__like-button" />
                 <button className="btn sm" id="share" onClick={handleShareClick}>
-                  <IoMdCopy /> Share
+                  <IoMdCopy />
                 </button>
-                {copied && <span className="copied-message">Copied</span>}
+
               </div>
             )}
+             {copied && <span className="copied-message">Copied Link</span>}
           </div>
           <h1 className="title">{post.title}</h1>
           <div className="post-detail__thumbnail">
