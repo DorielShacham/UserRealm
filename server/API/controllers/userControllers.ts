bcc786f909f4b10ebc658491dea4c07acfd90f1f
Doriel Shacham <97340087+DorielShacham@users.noreply.github.com>
2024-08-08 20:06:15 +0300

    Update userControllers.ts
    Final -_id

--- server/API/controllers/userControllers.ts
+++ server/API/controllers/userControllers.ts
@@ -100,7 +100,7 @@ const loginUser = async (req, res, next) => {
 const getUser = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const user = await UserModel.findById(id).select('avatar name posts role');
+    const user = await UserModel.findById(id).select('-_id avatar name posts role');
     // const user = await UserModel.findById(id).select("-password");
     if (!user) {
       return next(new HttpError("User not found", 404));
